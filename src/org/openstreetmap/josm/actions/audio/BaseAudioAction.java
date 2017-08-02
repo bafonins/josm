a99633fe68cac146d7ed52c626983ee6602c9e9e
Bogdans Afonins <bafonins@bafonins-macbook.local>
2017-08-02 18:52:10 +0200

    changed listener

--- src/org/openstreetmap/josm/actions/audio/BaseAudioAction.java
+++ src/org/openstreetmap/josm/actions/audio/BaseAudioAction.java
@@ -2,6 +2,7 @@ package org.openstreetmap.josm.actions.audio;
 
 import org.openstreetmap.josm.Main;
 import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.gui.layer.LayerManager;
 import org.openstreetmap.josm.gui.layer.markerlayer.AudioMarker;
 import org.openstreetmap.josm.gui.layer.markerlayer.MarkerLayer;
 import org.openstreetmap.josm.tools.Shortcut;
@@ -17,14 +18,33 @@ public abstract class BaseAudioAction extends JosmAction{
         setEnabled(isAudioMarkerPreset());
 
         // reevaluate presence of audio markers each time layouts change
-        Main.getLayerManager().addActiveLayerChangeListener(ev -> setEnabled(isAudioMarkerPreset()));
+        Main.getLayerManager().addLayerChangeListener(getLayerChangeListener());
     }
 
     protected boolean isAudioMarkerPreset() {
         return Main.getLayerManager().getLayers().stream()
-                    .filter(l -> l instanceof MarkerLayer)
-                    .map(ml -> (MarkerLayer) ml)
-                    .flatMap(ml -> Stream.of(ml.data))
-                    .anyMatch(m -> m instanceof AudioMarker);
+                .filter(l -> l instanceof MarkerLayer)
+                .map(ml -> (MarkerLayer) ml)
+                .flatMap(ml -> Stream.of(ml.data))
+                .anyMatch(m -> m instanceof AudioMarker);
+    }
+
+    private LayerManager.LayerChangeListener getLayerChangeListener() {
+        return new LayerManager.LayerChangeListener() {
+            @Override
+            public void layerAdded(LayerManager.LayerAddEvent e) {
+                setEnabled(isAudioMarkerPreset());
+            }
+
+            @Override
+            public void layerRemoving(LayerManager.LayerRemoveEvent e) {
+                setEnabled(isAudioMarkerPreset());
+            }
+
+            @Override
+            public void layerOrderChanged(LayerManager.LayerOrderChangeEvent e) {
+                // nothing
+            }
+        };
     }
 }
