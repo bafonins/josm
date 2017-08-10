274bacd8655afd9dbf29a8515ccd7ce9cdd981ee
Bogdans Afonins <bafonins@bafonins-macbook.local>
2017-08-10 15:27:45 +0200

    removed unused import

--- src/org/openstreetmap/josm/gui/download/OverpassQueryList.java
+++ src/org/openstreetmap/josm/gui/download/OverpassQueryList.java
@@ -18,7 +18,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
