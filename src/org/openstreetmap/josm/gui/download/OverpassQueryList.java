a6b17b0349afade9f86d81f977d4b3290415ef9d
Bogdans Afonins <bafonins@bafonins-macbook.local>
2017-08-11 18:37:31 +0200

    fixed default button index in ExtendedDialog

--- src/org/openstreetmap/josm/gui/download/OverpassQueryList.java
+++ src/org/openstreetmap/josm/gui/download/OverpassQueryList.java
@@ -48,6 +48,7 @@ import org.openstreetmap.josm.gui.widgets.DefaultTextComponentValidator;
 import org.openstreetmap.josm.gui.widgets.JosmTextArea;
 import org.openstreetmap.josm.gui.widgets.SearchTextResultListPanel;
 import org.openstreetmap.josm.tools.GBC;
+import org.openstreetmap.josm.tools.InputMapUtils;
 import org.openstreetmap.josm.tools.Utils;
 
 /**
@@ -186,7 +187,7 @@ public final class OverpassQueryList extends SearchTextResultListPanel<OverpassQ
                 componentParent,
                 tr("Edit item"),
                 item,
-                tr("Save"));
+                tr("Save"), tr("Cancel"));
         dialog.showDialog();
 
         Optional<SelectorItem> editedItem = dialog.getOutputItem();
@@ -480,8 +481,8 @@ public final class OverpassQueryList extends SearchTextResultListPanel<OverpassQ
             panel.add(this.name, GBC.eol().insets(5).anchor(GBC.SOUTHEAST).fill(GBC.HORIZONTAL));
             panel.add(queryScrollPane, constraint);
 
-            setDefaultButton(SUCCESS_BTN);
-            setCancelButton(CANCEL_BTN);
+            setDefaultButton(SUCCESS_BTN + 1);
+            setCancelButton(CANCEL_BTN + 1);
             setPreferredSize(new Dimension(400, 400));
             setContent(panel, false);
         }
